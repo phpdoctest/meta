ba68e5dd93ba71b8dcbf71dfed97eee6b9b4b0fd
Andreas Heigl <andreas@heigl.org>
2019-07-29 17:33:30 +0200

    Add Command to replace hashes in SVN-refs
    This command allows to replace a git hsah with another one without SVN
    complaining about changed hashes.
    
    To do so the hashes are replaced in the appropriate revmap file as well
    as in the logs/refs/remotes/origin/[branch], in the info/refs and in the
    packed-refs files

---
+++ src/Command/ReplaceHashForRevisionInSvnRefTable.php
@@ -0,0 +1,182 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright Andrea Heigl <andreas@heigl.org>
+ *
+ * Licenses under the MIT-license. For details see the included file LICENSE.md
+ */
+
+namespace PHPDocMeta\Command;
+
+use function bindec;
+use function fclose;
+use function feof;
+use function file;
+use function file_exists;
+use function file_get_contents;
+use function file_put_contents;
+use function filesize;
+use function filesWalk;
+use function fopen;
+use function fread;
+use function fwrite;
+use function getcwd;
+use function hex2bin;
+use function implode;
+use function parse_attr_string;
+use function parse_ini_file;
+use function preg_match;
+use function rename;
+use RuntimeException;
+use function sprintf;
+use function str_replace;
+use function strpos;
+use function strtolower;
+use function substr;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ReplaceHashForRevisionInSvnRefTable extends Command
+{
+    private const METADATAFILE = '/.git/svn/.metadata';
+
+    private const BRANCHREFFILE = '/.git/svn/refs/remotes/origin/%1$s/.rev_map.%2$s';
+
+    protected function configure()
+    {
+        $this->setName('replaceHashForRevisionInSvnRefTable')
+            ->setDescription('Replaces the hash for the given SVN-revision with the new hash')
+            ->setDefinition([
+                new InputOption('oldHash', 'o', InputOption::VALUE_REQUIRED, 'What is the old hash?'),
+                new InputOption('newHash', 'r', InputOption::VALUE_REQUIRED, 'What is the new hash?'),
+                new InputArgument('svnbranch', InputArgument::OPTIONAL, 'For which SVN-Branch shall we do the replacement?', 'trunk'),
+            ])
+            ->setHelp('');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        if (!file_exists(getcwd() . self::METADATAFILE)) {
+            throw new RuntimeException('No valid GIT-SVN directory.');
+        }
+
+        $info = parse_ini_file(getcwd() . self::METADATAFILE);
+
+        if (!isset($info['uuid'])) {
+            throw new RuntimeException('Something is wrong with your git-svn setup!');
+        }
+
+        $branchreffile = getcwd() . sprintf(
+                self::BRANCHREFFILE,
+                $input->getArgument('svnbranch'),
+                $info['uuid']
+            );
+        if (!file_exists($branchreffile)) {
+            throw new RuntimeException('No Branch-REference file found');
+        }
+
+        $replacedRevision = null;
+
+
+        $oldHash = $input->getOption('oldHash');
+        if (!preg_match('/[0-9a-fA-F]{40}/', $oldHash)) {
+            throw new RuntimeException('Old Hash is not a SHA1-hash');
+        }
+        $newHash = $input->getOption('newHash');
+        if (!preg_match('/[0-9a-fA-F]{40}/', $newHash)) {
+            throw new RuntimeException('New Hash is not a SHA1-hash');
+        }
+
+        $result = $this->replaceHashInRevMap($oldHash, $newHash, $branchreffile);
+        if ($result) {
+            $output->writeln($result);
+        }
+        $this->replaceHashInRefsInfo($oldHash, $newHash);
+        $this->replaceHashInRefLog($oldHash, $newHash, $input->getArgument('svnbranch'));
+        $this->replaceHashInPackedRefs($oldHash, $newHash);
+    }
+
+    private function replaceHashInRevMap($oldHash, $newHash, $branchreffile) : string
+    {
+        $oldHashBin = hex2bin($oldHash);
+        $newHashBin = hex2bin($newHash);
+        $replacedRevision = null;
+
+        if (file_exists($branchreffile . '.tmp')) {
+            throw new RuntimeException('lockfile already exists. Something went wrong the last time!');
+        }
+
+        rename($branchreffile, $branchreffile . '.tmp');
+        $base = fopen($branchreffile . '.tmp', 'r');
+        $new  = fopen($branchreffile, 'w');
+
+        while (! feof($base)) {
+            $content = fread($base, 24);
+            if (strpos($content, $oldHashBin) === 4) {
+                $replacedRevision = substr($content, 0, 4);
+                $content = $replacedRevision . $newHashBin;
+            }
+            fwrite($new, $content);
+        }
+
+        fclose($base);
+        fclose($new);
+
+        if (filesize($branchreffile) === filesize($branchreffile . '.tmp')) {
+        //    unlink($branchreffile . '.tmp');
+        }
+
+        if ($replacedRevision !== null) {
+            return sprintf('Replaced Hash for revision %1$s', bindec($replacedRevision));
+        }
+
+        return '';
+    }
+
+    public function replaceHashInRefsInfo($oldHash, $newHash) : void
+    {
+        $file = getcwd() . '/.git/info/refs';
+
+        $fileContent = file($file);
+        foreach ($fileContent as &$line) {
+            if (strpos($line, $oldHash) !== 0) {
+                continue;
+            }
+            $line = str_replace($oldHash, strtolower($newHash), $line);
+        }
+
+        file_put_contents($file, implode("", $fileContent));
+    }
+
+    public function replaceHashInPackedRefs($oldHash, $newHash) : void
+    {
+        $file = getcwd() . '/.git/packed-refs';
+
+        $fileContent = file($file);
+        foreach ($fileContent as &$line) {
+            if (strpos($line, $oldHash) !== 0) {
+                continue;
+            }
+            $line = str_replace($oldHash, strtolower($newHash), $line);
+        }
+
+        file_put_contents($file, implode("", $fileContent));
+    }
+
+    public function replaceHashInRefLog($oldHash, $newHash, $branch) : void
+    {
+        $file = getcwd() . '/.git/logs/refs/remotes/origin/' . $branch;
+
+        $fileContent = file($file);
+        foreach ($fileContent as &$line) {
+            $line = str_replace($oldHash, strtolower($newHash), $line);
+        }
+
+        file_put_contents($file, implode("", $fileContent));
+    }
+}
